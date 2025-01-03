2e49a6338dffdd4be2a32773deeb176f04b36900
Riku Kinjo <sansbook@icloud.com>
2025-01-03 22:12:24 +0900

    not test ok?

--- mypkg/year_count.py
+++ mypkg/year_count.py
@@ -1,21 +1,23 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import Int16,Float32
 import datetime
 
 
 #クラス
-#クラス内はみんな家族
 class Limit(Node):
     def __init__(self):
         super().__init__("year_limit")
         self.pub = self.create_publisher(Int16, "year_limit", 10)
+        self.pub_p = self.create_publisher(Float32, "year_percent", 10)
         self.timer = self.create_timer(1.0, self.year_limit)
+
+        self.origin_percent = None
 
 
     def calculation(self):
         now  = datetime.datetime.now()
-        new  = datetime.datetime(year=now.year,month=1,  day=1,  hour=1,  minute=1,  second=1 )
+        new  = datetime.datetime(year=now.year,month=1,  day=1,  hour=1,  minute=0,  second=0 )
         last = datetime.datetime(year=now.year,month=12, day=31, hour=23, minute=59, second=59)
         minute_limit =int((last - now).total_seconds())
 
@@ -29,12 +31,29 @@ class Limit(Node):
         result = self.calculation()
         limit = result[0]
         percent = result[1]
-        msg = Int16()
-        self.pub.publish(msg)
-        self.get_logger().info(f"1年の経過パーセンテージ:{percent:.2f}% | 残り時間(秒):{limit}")
 
+        msg_limit = Int16()
+        msg_limit.data = limit
+        self.pub.publish(msg_limit)
+
+        msg_percent = Float32()
+        msg_percent.data = percent
+        self.pub_p.publish(msg_percent)
+
+        if (self.origin_percent == None) or (abs(self.origin_percent - percent) > 0.0001):
+           self.get_logger().info(f"1年の経過パーセンテージ:{percent:.4f}% | 残り時間(秒):{limit}")
+           self.origin_percent = percent
+        else:
+           self.get_logger().info(f"                                | 残り時間(秒):{limit}")
 
 def main():
     rclpy.init()
     node = Limit()
-    rclpy.spin(node)
+#test.bashで強制終了する際の対処
+    try:
+       rclpy.spin(node)
+    except rclpy.executors.ExternalShutdownException:
+       pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
--- test/test.bash
+++ test/test.bash
@@ -7,3 +7,18 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 timeout 10 ros2 run mypkg year_count > /tmp/mypkg.log
+
+echo "Checking for year-end results..."
+ros2 topic echo /year_count std_msgs/msg/Int16 | grep "100.0000"  # or "残り時間(秒): 0"
+if [ $? -eq 0 ]; then
+    echo "Success: Year-end reached."
+else
+    echo "Failed: Year-end check failed."
+fi
+#[成功例]
+#年末のパーセントが100%か、残り時間が0秒か
+#年始のパーセントが  0%か、残り時間がMAX秒か
+#うるう年にも対応しているか、パーセントと残り時間
+
+#[失敗例]
+#打ち間違いによるエラー
